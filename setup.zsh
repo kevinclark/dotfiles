ba9da834416141772369483c17bf9724eb34ee5e
Kevin Clark <kevin.clark@gmail.com>
2020-11-12 12:28:08 -0800

    Cleanup plugin handling

--- setup.zsh
+++ setup.zsh
@@ -17,16 +17,16 @@ if ! [[ -d $OMZ_THEMES_DIR/powerlevel10k ]]; then
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $OMZ_THEMES_DIR/powerlevel10k
 fi
 
-if ! [[ -d $OMZ_PLUGINS_DIR/zsh-syntax-highlighting ]]; then
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $OMZ_PLUGINS_DIR/zsh-syntax-highlighting
-fi
+plugins=(
+  zsh-syntax-highlighting zsh-users/zsh-syntax-highlighting
+  op sirhc/op.plugin.zsh
+)
 
-if ! [[ -d $OMZ_PLUGINS_DIR/op ]]; then
-  git clone https://github.com/sirhc/op.plugin.zsh.git $OMZ_PLUGINS_DIR/op
-fi
-
-for f in `ls zsh`; do
-  ln -vsf $ROOT/zsh/$f ~/.$f
+for name gh in $plugins; do
+  plugin_dir=$OMZ_PLUGINS_DIR/$name
+  if ! [[ -d $plugin_dir ]]; then
+    git clone --depth=1 https://github.com/$gh $plugin_dir
+  fi
 done
 
 
